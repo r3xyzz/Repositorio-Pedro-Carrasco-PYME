ccf28549889b5bab7da491729c09ad6379e79c18
r3xyzz <rexgamer152@gmail.com>
2024-03-21 21:29:06 -0300

    cambio de fondo, info, logo, presentacion, animacion, enlaces

---
+++ animations.js
@@ -0,0 +1,7 @@
+/* animations.js */
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+
